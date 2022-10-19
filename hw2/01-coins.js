36ae5ad1201d85a233f733a0379a703e3cf4afbe
juhi <juh3@pdx.edu>
2022-10-19 16:46:27 -0700

    Added improvements

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -49,7 +49,6 @@ const calculateChange = (input) => {
   return result;
 };
 
-
 // Sample Test Cases
 console.log(calculateChange(4.62));
 // $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
@@ -59,7 +58,3 @@ console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
 console.log(calculateChange(15.11));
 // $15.11 ==> Error: the number is too large
- 
-
- 
-
