5d368cef8ad9fa1ac4471b0be6e4e7e950817e07
juhi <juh3@pdx.edu>
2022-10-19 16:41:22 -0700

    Formatted code

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,35 +1,34 @@
 /** Exercise 01 - Coins **/
 
 const calculateChange = (input) => {
+  if (input > 10) return "Error: the number is too large"
 
-if (input > 10) return "Error: the number is too large"
+  let amountInCents = input * 100; // 4.62 -> 462
 
-let amountInCents = input * 100; // 4.62 -> 462
+  let dollars = Math.floor(amountInCents/100); // 4
+  amountInCents = amountInCents - (dollars*100); // 62
 
-let dollars = Math.floor(amountInCents/100); // 4
-amountInCents = amountInCents - (dollars*100); // 62
+  let quarters = Math.floor(amountInCents/25); //2
+  amountInCents = amountInCents - (quarters * 25); // 12
 
-let quarters = Math.floor(amountInCents/25); //2
-amountInCents = amountInCents - (quarters * 25); // 12
+  let dimes = Math.floor(amountInCents/10); // 1
+  amountInCents = amountInCents - (dimes * 10); // 2
 
-let dimes = Math.floor(amountInCents/10); // 1
-amountInCents = amountInCents - (dimes * 10); // 2
+  let nickels = Math.floor(amountInCents/5); // 0
+  amountInCents = amountInCents - (nickels * 5); // 2
 
-let nickels = Math.floor(amountInCents/5); // 0
-amountInCents = amountInCents - (nickels * 5); // 2
+  let pennies = amountInCents;
+  
+  let dollarString = "";
+  let quarterString = "";
+  let dimeString = "";
+  let nickelString = "";
+  let penniesString = "";
 
-let pennies = amountInCents;
- 
- let dollarString = "";
- let quarterString = "";
- let dimeString = "";
- let nickelString = "";
- let penniesString = "";
-
- dollarString = dollars == 0 ? "" 
+  dollarString = dollars == 0 ? "" 
                 : dollars == 1 ? dollars.toString() + " dollar, "
                 : dollars.toString() + " dollars, ";
- 
+
   quarterString = quarters == 0 ? "" 
                 : quarters == 1 ? quarters.toString() + " quarter, "
                 : quarters.toString() + " quarters, ";
@@ -37,7 +36,7 @@ let pennies = amountInCents;
   dimeString = dimes == 0 ? "" 
                 : dimes == 1 ? dimes.toString() + " dime, "
                 : dimes.toString() + " dimes, ";
-  
+
   nickelString = nickels == 0 ? "" 
                 : nickels == 1 ? nickels.toString() + " nickel, "
                 : nickels.toString() + " nickels, ";
@@ -46,8 +45,8 @@ let pennies = amountInCents;
                 : pennies == 1 ? pennies.toString() + " penny"
                 : pennies.toString() + " pennies";
 
- result = dollarString + quarterString + dimeString + nickelString + penniesString;
- return result;
+  result = dollarString + quarterString + dimeString + nickelString + penniesString;
+  return result;
 };
 
 
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -4,19 +4,19 @@
 
 function submitForm(){
     console.log("========== Form Submission ===========");
-var username = document.getElementById("username").value;
-console.log("Name: " + username);
-var email = document.getElementById("email").value;
-console.log("Email: " + email);
-var feedback = document.getElementById("message").value;
-if (feedback == "")
-console.log("Feedback: No feedback was submitted");
-else
-console.log("Feedback: " + feedback);
-if (document.getElementById("exampleCheck1").checked)
-console.log("Newsletter: Yes, I would like to join the newletter");
-else
-console.log("Newsletter: No, thank you.");
+    var username = document.getElementById("username").value;
+    console.log("Name: " + username);
+    var email = document.getElementById("email").value;
+    console.log("Email: " + email);
+    var feedback = document.getElementById("message").value;
+    if (feedback == "")
+        console.log("Feedback: No feedback was submitted");
+    else
+        console.log("Feedback: " + feedback);
+    if (document.getElementById("exampleCheck1").checked)
+        console.log("Newsletter: Yes, I would like to join the newletter");
+    else
+        console.log("Newsletter: No, thank you.");
 };
 
 function resetForm(){
